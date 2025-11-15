414e67e1b8b72e61f1b32aa4993ced84a60302f3
jakkrapan Sreekajon <jsreekajon@gmail.com>
2025-11-15 18:56:39 +0700

    .env

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -4,14 +4,14 @@ import { getFirestore, setLogLevel } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCfmDRoVBHhhiO27zWO-vfmtkB2MKkND9Q",
-  authDomain: "weather-31ba2.firebaseapp.com",
-  databaseURL: "https://weather-31ba2-default-rtdb.firebaseio.com",
-  projectId: "weather-31ba2",
-  storageBucket: "weather-31ba2.firebasestorage.app",
-  messagingSenderId: "229233406375",
-  appId: "1:229233406375:web:59013c92957e3279308682",
-  measurementId: "G-KWV1Q5JET0"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL, // ถ้าคุณเพิ่มตัวนี้ใน .env
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
